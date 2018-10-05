7f26ff6e610f3cb052c9f92b97ee645f81d76a9c
Daniel Peixoto <danielspeixoto@outlook.com>
2018-10-05 00:24:55 -0300

    a

--- mysql/sql/indexes.sql
+++ mysql/sql/indexes.sql
@@ -1,3 +1,5 @@
 USE `qa`;
 
 CREATE INDEX parent ON posts(parent_id);
+CREATE INDEX parent ON posts(owner_user_id);
+CREATE INDEX related ON links(post_id);
--- postgresql/sql/indexes.sql
+++ postgresql/sql/indexes.sql
@@ -1,3 +1,5 @@
 \c `qa`;
 
 CREATE INDEX parent ON posts(parent_id);
+CREATE INDEX parent ON posts(owner_user_id);
+CREATE INDEX related ON links(post_id);
